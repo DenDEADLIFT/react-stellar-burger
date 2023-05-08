54ded6e7881f9b33630a7305fd9b4c77ff2d2403
DenDEADLIFT <115615232+DenDEADLIFT@users.noreply.github.com>
2023-05-08 10:12:31 +0000

    fix error

--- src/components/burgeringredients/ingridients/ingridients.jsx
+++ src/components/burgeringredients/ingridients/ingridients.jsx
@@ -3,24 +3,25 @@ import style from '../ingridients/ingridients.module.css';
 import { ingredientPropType } from '../../../utils/prop-types.js';
 
 function Ingridients(props) {
-    return (
-      <li className={style.ingridients_box} key={props._id}>
-        <img className={style.ingridients_image} src={props.src} alt={props.name} />
-        <div className={style.ingridients_prise_box}>
-          <p className="text text_type_digits-default">{props.price}</p>
-          <CurrencyIcon type="primary" />
-        </div>
-        <p className='mb-1 pb-5 text text_type_main-default'>{props.name}</p>
-        <Counter count={props.count} size="default" extraClass="m-1" />
-      </li>
-    );
-  }
+  return (
+    <li className={style.ingridients_box} key={props._id}>
+      <img className={style.ingridients_image} src={props.src} alt={props.name} />
+      <div className={style.ingridients_prise_box}>
+        <p className="text text_type_digits-default">{props.price}</p>
+        <CurrencyIcon type="primary" />
+      </div>
+      <p className='mb-1 pb-5 text text_type_main-default'>{props.name}</p>
+      <Counter count={props.count} size="default" extraClass="m-1" />
+    </li>
+  );
+}
 
-  Ingridients.propTypes = {
-    name: ingredientPropType.name,
-    price: ingredientPropType.price,
-    src: ingredientPropType.src,
-    _id: ingredientPropType._id,
-  };
+Ingridients.propTypes = {
+  name: ingredientPropType.name,
+  price: ingredientPropType.price,
+  src: ingredientPropType.src,
+  _id: ingredientPropType._id,
+  count: ingredientPropType.count
+};
 
-  export default Ingridients;
+export default Ingridients;
