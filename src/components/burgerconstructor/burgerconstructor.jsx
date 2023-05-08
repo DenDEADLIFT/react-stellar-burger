8d0d5611c49e83495c4708fc4255b52ed5b95bb7
DenDEADLIFT <115615232+DenDEADLIFT@users.noreply.github.com>
2023-05-08 08:14:22 +0000

    BurgerConstructor

--- src/components/burgerconstructor/burgerconstructor.jsx
+++ src/components/burgerconstructor/burgerconstructor.jsx
@@ -1,8 +1,73 @@
 import styles from "./burgerconstructor.module.css";
+import { data } from "../../utils/data.js";
+import {
+    Button,
+    ConstructorElement,
+    CurrencyIcon,
+    DragIcon,
+} from "@ya.praktikum/react-developer-burger-ui-components";
 
 function BurgerConstructor() {
     return (
-        <div className={`${styles.burger_constructor} custom-scroll`} />
+        <div className={`${styles.burger_constructor}`}>
+            <div style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
+                <div className={styles.item_box}>
+                    {data.map((i) => {
+                        if (i._id === "60666c42cc7b410027a1a9b1") {
+                            return (
+                                <ConstructorElement
+                                    type="top"
+                                    isLocked={true}
+                                    text="Краторная булка N-200i (верх)"
+                                    price={i.price}
+                                    thumbnail={i.image}
+                                />
+                            )
+                        }
+                    })}
+                </div>
+                <div className={`${styles.constructor_box} custom-scroll`}>
+                    {data.map((i) => {
+                        if (i.type !== 'bun') {
+                            return (
+                                <div className={`${styles.items}`}>
+                                    <DragIcon />
+                                    <ConstructorElement
+                                        text={i.name}
+                                        price={i.price}
+                                        thumbnail={i.image}
+                                    />
+                                </div>
+                            )
+                        }
+                    })}
+                </div>
+                <div className={styles.item_box}>
+                    {data.map((i) => {
+                        if (i._id === "60666c42cc7b410027a1a9b1") {
+                            return (
+                                <ConstructorElement
+                                    type="bottom"
+                                    isLocked={true}
+                                    text="Краторная булка N-200i (низ)"
+                                    price={i.price}
+                                    thumbnail={i.image}
+                                />
+                            )
+                        }
+                    })}
+                </div>
+            </div>
+            <ul className={styles.constructor_bottom}>
+                <li className={styles.constructor_price}>
+                    <p className="text text_type_digits-medium">610</p>
+                    <CurrencyIcon  type="primary" />
+                </li>
+                <li>
+                    <Button htmlType="button" type="primary" size="large">Оформить заказ</Button>
+                </li>
+            </ul>
+        </div>
     );
 }
 
