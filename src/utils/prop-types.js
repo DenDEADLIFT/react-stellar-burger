0ac22c7c80bd2980b9bf3be2b8fc915961224902
Denis Ivanov <site.ivanov201@yandex.ru>
2023-06-29 11:35:17 +0300

    login, logout, reset

--- src/utils/prop-types.js
+++ src/utils/prop-types.js
@@ -1,7 +1,6 @@
 import PropTypes from "prop-types";
 
 export const ingredientPropType = PropTypes.shape({
-  //укажите здесь prop-types для ингридиента
   _id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   type: PropTypes.oneOf(['bun', 'main', 'sauce']).isRequired,
